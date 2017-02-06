4785c5f671e0a9e8f3ac457c1fc4a1e788cb9b45
Awasson10 <Awasson10@knights.ucf.edu>
2017-02-05 21:05:34 -0500

    Create a1p4.py

---
+++ a1p4.py
@@ -0,0 +1,11 @@
+#problem 4
+#Asks the user for two random numbers and perfroms the four basic math operations
+x= input("give me a number ")
+y= input("give me another number ")
+X=int(x)
+Y=int(y)
+#pritns the four basic operations from the above random numbers inputed by the user.
+print (X, " + ", Y, " = ", X+Y)
+print (X, " - ", Y, " = ", X-Y)
+print (X, " * ", Y, " = ", X*Y)
+print (X, " / ", Y, " = ", X/Y)
