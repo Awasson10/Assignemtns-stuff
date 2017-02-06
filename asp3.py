29659de9e4a013aa2c658d754eda6c997f087cc1
Awasson10 <Awasson10@knights.ucf.edu>
2017-02-05 21:04:43 -0500

    Create asp3.py

---
+++ asp3.py
@@ -0,0 +1,8 @@
+#Problem 3
+#Program asks the usrer for the following, noun, verb, adverb, adjective, and puts them into a phrase
+noun= input("give me a noun ")
+verb =input("give me a verb ")
+adverb = input("give me an adverb ")
+adjective= input("give me an adjective ")
+#prints out the phrase below, and congrats the user on being funny.
+print("did you", verb,"your", adjective, noun, adverb,"?.", "Congrats your as funny as a one legged pirate")
